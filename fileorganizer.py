413297dd498d769025fa2d6f020e5ded320f18f4
Kubata <121972929+fabiokubata@users.noreply.github.com>
2025-01-22 22:14:44 -0300

    Add files via upload

---
+++ fileorganizer.py
@@ -0,0 +1,23 @@
+import os
+from tkinter.filedialog import askdirectory
+
+caminho = askdirectory(title="Selecione uma pasta")
+
+lista_arquivos = os.listdir(caminho)
+
+locais = {
+    "Imagens": [".png", ".jpg", ".JPG", ".bmp", ".heic",".jpeg"],
+    "Planilhas": [".xlsx", ".csv"],
+    "Pdfs": [".pdf"],
+    "Audio": [".mp3"],
+    "Video": [".mp4", ".wav"],
+    "Texto": [".docx", ".txt"],
+}
+
+for arquivo in lista_arquivos:
+    nome, extensao = os.path.splitext(f"{caminho}/{arquivo}")
+    for pasta in locais:
+        if extensao in locais[pasta]:
+            if not os.path.exists(f"{caminho}/{pasta}"):
+                os.mkdir(f"{caminho}/{pasta}")
+            os.rename(f"{caminho}/{arquivo}", f"{caminho}/{pasta}/{arquivo}")
